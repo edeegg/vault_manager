63497c6557efef788a418eae28461dacd11033ab
edeegg <edilsonmauro67@gmail.com>
2022-09-22 14:04:14 -0300

    create vault content entity

---
+++ lib/app/modules/vault/domain/entities/vault_content_entity.dart
@@ -0,0 +1,10 @@
+class VaultContentEntity {
+  final String serviceName;
+  final String serviceEmail;
+  final String serviceImageUrl;
+  VaultContentEntity({
+    required this.serviceName,
+    required this.serviceEmail,
+    required this.serviceImageUrl,
+  });
+}
