92c4b671236e2f83c6f18182cddfda3a4c9a5dc0
edeegg <edilsonmauro67@gmail.com>
2022-09-22 13:47:46 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/vault_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vault_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind<Object>> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [];
+}
